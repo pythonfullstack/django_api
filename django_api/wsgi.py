b61ccf5848b6f17a0d10c737cae6dbff6bb59af0
pythonfullstack <chenfang199531@gmail.com>
2021-10-18 22:00:40 -0400

    update: wsgi

--- django_api/wsgi.py
+++ django_api/wsgi.py
@@ -7,21 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_api.settings')
-#
-# application = get_wsgi_application()
-
-
 import os
-import django
-from django.core.handlers.wsgi import WSGIHandler
 
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_api.settings')
-django.setup(set_prefix=False)
 
-application = WSGIHandler()
+application = get_wsgi_application()
